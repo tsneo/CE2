501a89c4d903bcef614f3ece15e3d43b4d05c6b4
TS <ts_neo89@hotmail.com>
2015-02-25 11:37:25 +0800

    Aligned the codes

--- src/TextBuddy.java
+++ src/TextBuddy.java
@@ -54,7 +54,6 @@ public class TextBuddy {
 	// This arraylist will store the list of texts
 	private static ArrayList<String> strList = new ArrayList<String>();
 
-
 	// List of Display Messages for welcome message, add, delete, clear and file
 	// empty
 	private static final String MESSAGE_WELCOME = "Welcome to TextBuddy. %1$s is ready to use";
@@ -74,7 +73,9 @@ public class TextBuddy {
 
 	/**
 	 * This is where the program will begin
-	 * @param args expect filename to be received here
+	 * 
+	 * @param args
+	 *            expect filename to be received here
 	 */
 	public static void main(String[] args) {
 
@@ -91,7 +92,9 @@ public class TextBuddy {
 	/**
 	 * This method set up the Command Interface and exit if the user enter exit
 	 * command
-	 * @param fileName an filename for saving
+	 * 
+	 * @param fileName
+	 *            an filename for saving
 	 */
 	private static void commandExecutionUntilExit(String fileName) {
 		String[] inputCmd;
@@ -103,14 +106,17 @@ public class TextBuddy {
 			System.out.print("command: ");
 		}
 	}
-	
+
 	/**
 	 * Execute command and return the message
-	 * @param fileName an filename for saving
-	 * @param cmd commands with parameters to determine which to execute
+	 * 
+	 * @param fileName
+	 *            an filename for saving
+	 * @param cmd
+	 *            commands with parameters to determine which to execute
 	 * @return
 	 */
-	public static String executeCommand(String fileName, String[] cmd){
+	public static String executeCommand(String fileName, String[] cmd) {
 		Command_Types commandType = determineCommandType(cmd);
 
 		switch (commandType) {
@@ -138,10 +144,11 @@ public class TextBuddy {
 		return "No command";
 	}
 
-	
 	/**
 	 * This method determine whether it is a command type
-	 * @param commandTypeString contains the commands which will be determined
+	 * 
+	 * @param commandTypeString
+	 *            contains the commands which will be determined
 	 * @return and Command_Types such as ADD, DELETE and etc
 	 */
 	private static Command_Types determineCommandType(String[] commandTypeString) {
@@ -169,10 +176,11 @@ public class TextBuddy {
 
 	}
 
-	
 	/**
 	 * Check if file exists and load it to a list
-	 * @param args receive argument which will contains the filename
+	 * 
+	 * @param args
+	 *            receive argument which will contains the filename
 	 * @return the filename if the argument fulfill the condition
 	 */
 	public static String checkAndLoadFile(String[] args) {
@@ -200,14 +208,15 @@ public class TextBuddy {
 				printErrorMessageAndExit(e.toString());
 			}
 		}
-		
+
 		return fileName;
 	}
 
-
 	/**
 	 * This method will check whether there is an argument input
-	 * @param args check the argument length
+	 * 
+	 * @param args
+	 *            check the argument length
 	 */
 	private static void exitIfNoArgument(String[] args) {
 		if (args.length == 0) {
@@ -215,11 +224,12 @@ public class TextBuddy {
 		}
 	}
 
-
 	/**
-	 * This method will check if the file is in correct format
-	 * and exit if incorrect
-	 * @param fileName the string to compare
+	 * This method will check if the file is in correct format and exit if
+	 * incorrect
+	 * 
+	 * @param fileName
+	 *            the string to compare
 	 */
 	private static void exitIfWrongFileFormat(String fileName) {
 		boolean isFileContainsADot = fileName.contains(".");
@@ -236,7 +246,9 @@ public class TextBuddy {
 
 	/**
 	 * This method will load the contents from the text file to ArrayList
-	 * @param fileName the filename which will be opened and load contents into
+	 * 
+	 * @param fileName
+	 *            the filename which will be opened and load contents into
 	 */
 	private static void loadToArrayList(String fileName) {
 		try {
@@ -256,18 +268,21 @@ public class TextBuddy {
 		}
 	}
 
-
 	/**
 	 * Add text to file
-	 * @param fileName an filename for saving
-	 * @param inputArr the command and the text are inside this param
-	 * @return as a String. No text to add if command is in wrong format or success.
+	 * 
+	 * @param fileName
+	 *            an filename for saving
+	 * @param inputArr
+	 *            the command and the text are inside this param
+	 * @return as a String. No text to add if command is in wrong format or
+	 *         success.
 	 */
 	private static String addText(String fileName, String[] inputArr) {
-		if(inputArr.length < 2){
+		if (inputArr.length < 2) {
 			return "No text to add";
 		}
-		
+
 		String txtToAdd = "";// declare a string to put the words into it
 
 		// loop and write it to txtToAdd
@@ -275,7 +290,7 @@ public class TextBuddy {
 			txtToAdd += inputArr[i];// append or add on
 
 			// check if can add a space after the last letter
-			if ((i + 1) != inputArr.length){
+			if ((i + 1) != inputArr.length) {
 				txtToAdd += " ";
 			}
 		}
@@ -291,94 +306,107 @@ public class TextBuddy {
 
 	/**
 	 * This method will add text to arraylist
-	 * @param text the text from the command line
+	 * 
+	 * @param text
+	 *            the text from the command line
 	 */
 	private static void addToArrayList(String text) {
 		strList.add(text);
 	}
 
-
 	/**
 	 * This method will remove a line from the text file
-	 * @param inputCmd the command and the index number for deletion
-	 * @return as a String. Invalid if command is in wrong format, success message or no such item.
+	 * 
+	 * @param inputCmd
+	 *            the command and the index number for deletion
+	 * @return as a String. Invalid if command is in wrong format, success
+	 *         message or no such item.
 	 */
 	private static String deleteText(String fileName, String[] inputCmd) {
-		if (inputCmd.length != 2 || !isStringAnInteger(inputCmd[1])){
+		if (inputCmd.length != 2 || !isStringAnInteger(inputCmd[1])) {
 			return "Invalid arguments";
 		}
-			
+
 		int listIndex = Integer.parseInt(inputCmd[1]);
 
 		/*
 		 * This checks for the position of the arraylist
 		 */
-		if ((listIndex -1) >= 0 && (listIndex-1) < strList.size()) {
+		if ((listIndex - 1) >= 0 && (listIndex - 1) < strList.size()) {
 			String removedText = strList.get(listIndex - 1);
 			strList.remove(listIndex - 1);
 			writeToFile(fileName);
 			return String.format(MESSAGE_DELETED, fileName, removedText);
 		}
-		
+
 		return "No such item exist";
 
 	}
-	
+
 	/**
 	 * This method check if the given string can be converted to integer
-	 * @param inputStr the string to be converted
+	 * 
+	 * @param inputStr
+	 *            the string to be converted
 	 * @return true if can be converted, else false
 	 */
-	private static boolean isStringAnInteger(String inputStr){
-		try { 
-	        Integer.parseInt(inputStr); 
-	    } catch(NumberFormatException e) { 
-	        return false; 
-	    }
-		
+	private static boolean isStringAnInteger(String inputStr) {
+		try {
+			Integer.parseInt(inputStr);
+		} catch (NumberFormatException e) {
+			return false;
+		}
+
 		return true;
 	}
 
 	/**
 	 * This will display the list of text stored in the file
-	 * @param fileName the filename which use to display as a text
-	 * @param cmd the command input for verification of the command
+	 * 
+	 * @param fileName
+	 *            the filename which use to display as a text
+	 * @param cmd
+	 *            the command input for verification of the command
 	 * @return a list of text
 	 */
 	private static String display(String fileName, String[] cmd) {
-		if(cmd.length != 1){
+		if (cmd.length != 1) {
 			return "Invalid command for display. Please remove the contents after the word 'display'";
 		}
-		
-		if (strList.isEmpty()){
+
+		if (strList.isEmpty()) {
 			return String.format(MESSAGE_FILEISEMPTY, fileName);
 		}
-		
+
 		String displayText = new String();
-		
+
 		for (int i = 0; i < strList.size(); i++) {
 			displayText += (i + 1) + ". " + strList.get(i).toString();
-			if((i+1) != strList.size()){
+			if ((i + 1) != strList.size()) {
 				displayText += "\n";
 			}
 		}
-		
+
 		return displayText;
 	}
 
 	/**
 	 * This method clear all the contents
-	 * @param fileName an filename for saving
-	 * @param cmd contains the clear command
-	 * @return as a String. Invalid if command is in wrong format, success message if success.
+	 * 
+	 * @param fileName
+	 *            an filename for saving
+	 * @param cmd
+	 *            contains the clear command
+	 * @return as a String. Invalid if command is in wrong format, success
+	 *         message if success.
 	 */
 	public static String clearContents(String fileName, String[] cmd) {
-		if(cmd.length != 1){
+		if (cmd.length != 1) {
 			return "Invalid command for clear. Please remove the contents after the word 'clear'";
 		}
-		
+
 		clearArrayList();
-		
+
 		try {
 			FileWriter fw = new FileWriter(fileName);// setup a file writer with
 														// nothing inside
@@ -387,20 +415,22 @@ public class TextBuddy {
 			printErrorMessageAndExit(e.toString());
 			return "Failed to clear";
 		}
-		
+
 		return String.format(MESSAGE_CLEARED, fileName);
 	}
-	
+
 	/**
 	 * This method clear the contents from the AL
 	 */
-	private static void clearArrayList(){
+	private static void clearArrayList() {
 		strList.clear();
 	}
 
 	/**
 	 * This method write the contents to the text file
-	 * @param fileName an filename for saving
+	 * 
+	 * @param fileName
+	 *            an filename for saving
 	 */
 	private static void writeToFile(String fileName) {
 		// Add the string to the file
@@ -419,84 +449,96 @@ public class TextBuddy {
 		}
 	}
 
-
 	/**
 	 * This method prints out the string with the given parameter as text
-	 * @param message the message from every commands returned
+	 * 
+	 * @param message
+	 *            the message from every commands returned
 	 */
 	private static void printMessage(String message) {
 		System.out.println(message);
 	}
 
-
 	/**
 	 * This method will print out the error message before exiting the system
-	 * @param message the message from various methods
+	 * 
+	 * @param message
+	 *            the message from various methods
 	 */
 	private static void printErrorMessageAndExit(String message) {
 		System.out.println(message);
 		System.exit(0);
 	}
-	
+
 	/**
-	 * This method will search for the word and return the list of text
-	 * The search criteria will be if the given search string contains inside the list of text
-	 * The search will only search 1 word
-	 * @param cmd contains the command search with the search criteria
-	 * @return a list of text as a string. Invalid if command is in wrong format else no matches.
+	 * This method will search for the word and return the list of text The
+	 * search criteria will be if the given search string contains inside the
+	 * list of text The search will only search 1 word
+	 * 
+	 * @param cmd
+	 *            contains the command search with the search criteria
+	 * @return a list of text as a string. Invalid if command is in wrong format
+	 *         else no matches.
 	 */
-	private static String searchAndReturnList(String[] cmd){
-		if (cmd.length != 2){
+	private static String searchAndReturnList(String[] cmd) {
+		if (cmd.length != 2) {
 			return "Invalid arguments for search. Please enter a word after the 'search' word.";
 		}
-		
+
 		String searchWord = cmd[1];
 		String filteredLines = new String();
 		int indexNum = 1;
-		for(int i = 0; i < strList.size(); i++){
-			if(compareString(searchWord, strList.get(i))){
+		for (int i = 0; i < strList.size(); i++) {
+			if (compareString(searchWord, strList.get(i))) {
 				filteredLines += indexNum + ". " + strList.get(i) + "\n";
 				indexNum++;
 			}
 		}
-		if(filteredLines.isEmpty()){
+		if (filteredLines.isEmpty()) {
 			return "No matches";
 		}
-		
-		filteredLines = filteredLines.substring(0, filteredLines.length() -1);
-		
+
+		filteredLines = filteredLines.substring(0, filteredLines.length() - 1);
+
 		return filteredLines;
 	}
-	
+
 	/**
-	 * This method compare the string with the given word and return
-	 * true if there is a match, false if not
-	 * @param searchWord the string to compare
-	 * @param compareWord the string to compare with
+	 * This method compare the string with the given word and return true if
+	 * there is a match, false if not
+	 * 
+	 * @param searchWord
+	 *            the string to compare
+	 * @param compareWord
+	 *            the string to compare with
 	 * @return true for matched, else false
 	 */
-	private static boolean compareString(String searchWord, String compareWord){
+	private static boolean compareString(String searchWord, String compareWord) {
 		return compareWord.toLowerCase().contains(searchWord.toLowerCase());
 	}
-	
+
 	/**
 	 * This method sort the arrayList
-	 * @param fileName an filename for saving
-	 * @param cmd contains sort command
-	 * @return as a String. Invalid if command is in wrong format, no item and success message.
+	 * 
+	 * @param fileName
+	 *            an filename for saving
+	 * @param cmd
+	 *            contains sort command
+	 * @return as a String. Invalid if command is in wrong format, no item and
+	 *         success message.
 	 */
-	private static String sortArrayList(String fileName, String[] cmd){
-		if(cmd.length != 1){
+	private static String sortArrayList(String fileName, String[] cmd) {
+		if (cmd.length != 1) {
 			return "Invalid command for sort. Please remove the contents after the word 'sort'";
 		}
-		
-		if(strList.size() == 0){
+
+		if (strList.size() == 0) {
 			return "There is no items to sort";
 		}
-		
+
 		Collections.sort(strList);
 		writeToFile(fileName);
-		
+
 		return "Sort complete";
 	}
 }
